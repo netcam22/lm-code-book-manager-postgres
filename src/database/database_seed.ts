d62b36a8ed9dcaf85da846b6bb7b8e47de2fdd4c
netcam22 <annettelesage@btinternet.com>
2023-11-26 13:46:42 +0000

    feat: add extra book to seed file

--- src/database/database_seed.ts
+++ src/database/database_seed.ts
@@ -17,6 +17,13 @@ export const populateDummyData = async () => {
 		description:
 			"Before being born, each person must visit the magical Shop Before Life, where they choose what kind of person they will become down on Earth...",
 	});
+	await Book.create({
+		bookId: 3,
+		title: "The Handmaid's Tale",
+		author: "Margaret Atwood",
+		description:
+			"A futuristic dystopian novel by the Canadian author published in 1985.",
+	});
 	const bookCount = (await Book.findAll()).length;
 	console.log(
 		`📚 ${bookCount} book${bookCount !== 1 ? "s" : ""} added to table`
